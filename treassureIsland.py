f0e7a4baaf264ca4ca0c86903d49d46c911903d7
Varadaraj <vr20aaj@outlook.com>
2021-11-11 16:46:29 +0000

    Update treassureIsland.py

--- treassureIsland.py
+++ treassureIsland.py
@@ -20,20 +20,20 @@ ____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
 /______/______/______/______/______/______/______/______/______/______/[TomekK]
 ******************************************************************************* ''')
 print("Welcome to the treasure island!")
-choice1=input("You are at a cross road to find a treasure now choose which way you would like to go either Left or Right?\n")
+choice1=input("You are at a cross road to find a treasure now choose which way you would like to go either Left or Right?\n").lower()
 
-if choice1=="Left":
-    choice2=input("You have arried at sea please select how would like to go to the Island>? Swim or Wait\n")
-    if choice2=="Swim":
+if choice1=="left":
+    choice2=input("You have arried at sea please select how would like to go to the Island>? Swim or Wait\n").lower()
+    if choice2=="swim":
         print("You are attacked by a blue wale you are dead, Game Over!")
     if choice2== "wait":
         print("You have reached the Island, there is a house with three doors with Blue, Red, Yellow")
-        choice3=input("please select which door you want to open to see the treasure? Blue, Red, Yellow:\n")
-        if choice3=="Blue":
+        choice3=input("please select which door you want to open to see the treasure? Blue, Red, Yellow:\n").lower()
+        if choice3=="blue":
             print("Game over! You fell into a hole")
-        elif choice3=="Red":
+        elif choice3=="red":
             print("Game Over! You entered into a room full of fire.")
-        elif choice3=="Yellow":
+        elif choice3=="yellow":
             print("Hurray! you have found the treasure and you win!")
         else:
             print("Game Over! please choose the Right option and try again to find the treasure.")
